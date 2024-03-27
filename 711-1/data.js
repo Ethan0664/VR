a92e112efba1d0cee0ba1099ebd0463019befc3c
Ethan0664 <ethandu0604@gmail.com>
2024-03-27 14:29:45 +0800

    push

---
+++ 711-1/data.js
@@ -0,0 +1,278 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "門口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -0.09891577042087363,
+        "pitch": -0.3056827292028004,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2014239829439255,
+          "pitch": 0.33748106823339086,
+          "rotation": 12.566370614359176,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.651077642814478,
+          "pitch": 0.4021306249193053,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.9037504446546585,
+          "pitch": 0.7590689895155975,
+          "rotation": 6.283185307179586,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.2208568553964163,
+          "pitch": -0.13298004582838097,
+          "title": "松高門市特色",
+          "text": "<span style=\"font-size: 16px;\">主題式便利商店，充滿許多咖波相關商品</span>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "櫃台",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -1.8665384923639259,
+        "pitch": 0.26424011446571427,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6358563158098267,
+          "pitch": 0.5726796786489867,
+          "rotation": 6.283185307179586,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.0019216971042688158,
+          "pitch": 0.6025250199616714,
+          "rotation": 0.7853981633974483,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.518466104390459,
+          "pitch": -0.1275481182333884,
+          "title": "充滿特色的牆壁",
+          "text": "除了相當多的連動商品外，牆上的裝飾也是十分有特色"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "商品區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 1.962436849789544,
+        "pitch": 0.5650247759013247,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7076616271201406,
+          "pitch": 0.47152963517035396,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.9854395355072167,
+          "pitch": 0.41597293760495546,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.34354348884996533,
+          "pitch": 0.7010029330728837,
+          "rotation": 5.497787143782138,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.0002509391721253,
+          "pitch": 0.573018948775978,
+          "title": "商品區",
+          "text": "除了連動商品，普通的商品也是應有盡有"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "飲料區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.3334563630911127,
+        "pitch": 0.2352862561238105,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.259604444920077,
+          "pitch": 0.6697434073076174,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 2.005478071461641,
+          "pitch": 0.4853546852062607,
+          "rotation": 0.7853981633974483,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1301996568740194,
+          "pitch": 0.264517198694012,
+          "title": "飲料冰櫃",
+          "text": "充滿各式的飲料，夏天解暑的寶庫"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "座位區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.29600626000326713,
+        "pitch": 0.3437855817573858,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5635956872834775,
+          "pitch": 0.6868775308688555,
+          "rotation": 0.7853981633974483,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.24802226125054538,
+          "pitch": 0.5165160255614332,
+          "rotation": 0.7853981633974483,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7046168258905148,
+          "pitch": 0.6572199909470946,
+          "title": "座位區",
+          "text": "提供休息與吃飯的場所，同樣包含滿滿的咖波元素"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
